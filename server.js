ac56a7655d4a06dd8924ed70088b78a3c641b263
klancyk3 <jozef.lyscarz@gmail.com>
2026-08-14 13:53:40 +0200

    port 5174

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 5174;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
